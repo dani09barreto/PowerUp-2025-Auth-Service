7c48d92a66aead3500803465908245a1334aaf06
dani09barreto <danielf.barreto@javeriana.edu.co>
2025-09-05 20:34:58 -0500

    delete login use case

--- domain/usecase/src/main/java/co/com/pragma/bootcamp/auth/usecase/loginuser/ILoginUserUseCase.java
+++
@@ -1,8 +0,0 @@
-package co.com.pragma.bootcamp.auth.usecase.loginuser;
-
-import co.com.pragma.bootcamp.auth.model.token.Token;
-import reactor.core.publisher.Mono;
-
-public interface ILoginUserUseCase {
-    Mono<Token> login(String email, String password);
-}
--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/bootcamp/auth/api/auth/AuthHandler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/bootcamp/auth/api/auth/AuthHandler.java
@@ -3,14 +3,18 @@ package co.com.pragma.bootcamp.auth.api.auth;
 import co.com.pragma.bootcamp.auth.api.dto.TokenResponse;
 import co.com.pragma.bootcamp.auth.api.dto.UserLoginRequest;
 import co.com.pragma.bootcamp.auth.api.error.ApiError;
-import co.com.pragma.bootcamp.auth.usecase.loginuser.ILoginUserUseCase;
+import co.com.pragma.bootcamp.auth.model.role.gateways.IRoleRepository;
+import co.com.pragma.bootcamp.auth.model.token.Token;
+import co.com.pragma.bootcamp.auth.model.token.gateways.TokenRepository;
+import co.com.pragma.bootcamp.auth.model.user.gateways.IUserRepository;
+import co.com.pragma.bootcamp.auth.usecase.error.InvalidUserCredentialsException;
+import co.com.pragma.bootcamp.auth.usecase.error.UserNotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -21,7 +25,9 @@ import reactor.core.publisher.Mono;
 @RequiredArgsConstructor
 public class AuthHandler {
 
-    private final ILoginUserUseCase loginUserUseCase;
+    private final IUserRepository userRepository;
+    private final TokenRepository tokenRepository;
+    private final IRoleRepository roleRepository;
 
     @Operation(
             summary = "Autenticar usuario",
@@ -49,7 +55,22 @@ public class AuthHandler {
         log.info("Received request to login user");
 
         return serverRequest.bodyToMono(UserLoginRequest.class)
-                .flatMap(userLoginRequest -> loginUserUseCase.login(userLoginRequest.email(), userLoginRequest.password()))
+                .flatMap(userLoginRequest -> this.login(userLoginRequest.email(), userLoginRequest.password()))
                 .flatMap(authResponse -> ServerResponse.ok().bodyValue(authResponse));
+    }
+
+    private Mono<Token> login(String email, String password) {
+        return userRepository.findByEmail(email)
+                .flatMap(user -> roleRepository.getRoleById(user.getRole().getId())
+                        .map(role -> {
+                            user.setRole(role);
+                            return user;
+                        }))
+                .switchIfEmpty(Mono.error(new UserNotFoundException("User not found with email: " + email)))
+                .filter(user -> tokenRepository.passwordEncoderMatches(password, user.getPassword()))
+                .map(tokenRepository::generateToken)
+                .map(Token::new)
+                .log()
+                .switchIfEmpty(Mono.error(new InvalidUserCredentialsException("Invalid credentials")));
     }
 }
