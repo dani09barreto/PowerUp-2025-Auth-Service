a13349b9f3a838085517af6acbc8dd9937236350
dani09barreto <danielf.barreto@javeriana.edu.co>
2025-09-08 18:47:50 -0500

    added error file desc

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/bootcamp/auth/api/error/ErrorFilter.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/bootcamp/auth/api/error/ErrorFilter.java
@@ -12,7 +12,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.*;
 import reactor.core.publisher.Mono;
 
-import java.nio.file.AccessDeniedException;
 import java.time.LocalDateTime;
 
 @Slf4j
@@ -23,22 +22,23 @@ public class ErrorFilter implements HandlerFilterFunction<ServerResponse, Server
     public Mono<ServerResponse> filter(ServerRequest request, HandlerFunction<ServerResponse> next) {
         return next.handle(request)
                 .onErrorResume(InvalidUserDataException.class,
-                        ex -> buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), request))
+                        ex -> buildErrorResponse(HttpStatus.BAD_REQUEST, ex.getMessage(), request, ex.getStackTrace()))
                 .onErrorResume(InvalidUserCredentialsException.class,
-                        ex -> buildErrorResponse(HttpStatus.UNAUTHORIZED, ex.getMessage(), request))
+                        ex -> buildErrorResponse(HttpStatus.UNAUTHORIZED, ex.getMessage(), request, ex.getStackTrace()))
                 .onErrorResume(UserAlreadyExistsException.class,
-                        ex -> buildErrorResponse(HttpStatus.CONFLICT, ex.getMessage(), request))
+                        ex -> buildErrorResponse(HttpStatus.CONFLICT, ex.getMessage(), request, ex.getStackTrace()))
                 .onErrorResume(UserNotFoundException.class,
-                        ex -> buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request))
+                        ex -> buildErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage(), request, ex.getStackTrace()))
                 .onErrorResume(AuthorizationDeniedException.class,
-                        ex -> buildErrorResponse(HttpStatus.FORBIDDEN, ex.getMessage(), request))
+                        ex -> buildErrorResponse(HttpStatus.FORBIDDEN, ex.getMessage(), request, ex.getStackTrace()))
                 .onErrorResume(Exception.class,
-                        ex -> buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), request));
+                        ex -> buildErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex.getMessage(), request, ex.getStackTrace()));
 
     }
 
-    private Mono<ServerResponse> buildErrorResponse(HttpStatus status, String message, ServerRequest request) {
+    private Mono<ServerResponse> buildErrorResponse(HttpStatus status, String message, ServerRequest request, StackTraceElement[] stackTrace) {
         log.error("Error occurred: {}", message);
+        log.debug("Stack trace: ", new Throwable().initCause(new Exception()).fillInStackTrace());
         ApiError apiError = ApiError.builder()
                 .timestamp(LocalDateTime.now())
                 .status(status.value())
